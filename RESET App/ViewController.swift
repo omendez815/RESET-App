8c2aee0bd13f486796c4ca2a0b84781af3827349
Omar Mendez <omendez815@gmail.com>
2020-05-21 19:06:48 -0500

    Initial Commit

---
+++ RESET App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RESET App
+//
+//  Created by Omar Mendez on 5/21/20.
+//  Copyright © 2020 Omar Mendez and Anna Woolridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
