1eace82b2c8016188583e9c7902803eb320378b9
Anna Woolridge <anna4yellow@gmail.com>
2020-05-22 18:24:46 -0700

    Friday 5/22

--- RESET App/ViewController.swift
+++ RESET App/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //First comment - testing github
+        //NEW Friday 5/22 comment
     }
 
 
